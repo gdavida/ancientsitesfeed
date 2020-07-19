8f1aeff51adf0e26b7c9e9df19ce5160675eeef4
Davida Gaffney <gdavida@gmail.com>
2020-07-19 11:43:09 -0500

    remove 2nd object and see if this helps

--- index.js
+++ index.js
@@ -18,14 +18,6 @@ server.get('/', (req, res) => {
 	            "mainText": "Today your tour brings you to Grimes Graves, a large flint mining complex in Norfolk, England. These mines were worked between 2600 and 2300 BC. Flint was in high demand during this time for use in polished stone axes. The mines extend over 91 acres with 433 shafts, some more than 46 feet deep and 39 feet in diameter.",
 	            "streamUrl": "",
 	            "redirectionUrl": ""
-	        },  
-	        {
-	            "uid": "660de062-65c1-4bbb-924b-8e84cfa3b119",
-	            "updateDate": "2020-07-16T08:20:00.0Z",
-	            "titleText": "Let's visit somewhere else.",
-	            "mainText": "Beep boop bop boop beep boop boop.",
-	            "streamUrl": "",
-	            "redirectionUrl": ""
 	        }
 	    ]
 	)
