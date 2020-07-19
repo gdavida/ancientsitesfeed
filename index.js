187bcc4fc646fc8f01ef20bb64b480216a0a9746
Davida Gaffney <gdavida@gmail.com>
2020-07-18 23:32:38 -0500

    remove streamURL

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ server.get('/', (req, res) => {
 	            "updateDate": "2020-07-16T23:18:00.0Z",
 	            "titleText": "Let's visit Grimes Graves in England.",
 	            "mainText": "Today your tour brings you to Grimes Graves, a large flint mining complex in Norfolk, England. These mines were worked between 2600 and 2300 BC. Flint was in high demand during this time for use in polished stone axes. The mines extend over 91 acres with 433 shafts, some more than 46 feet deep and 39 feet in diameter.",
-	            "streamUrl": "",
 	            "redirectionUrl": "www.storage-mart.com"
 	        },  
 	        {
@@ -24,7 +23,6 @@ server.get('/', (req, res) => {
 	            "updateDate": "2020-07-18T08:20:00.0Z",
 	            "titleText": "Let's visit somewhere else.",
 	            "mainText": "Beep boop bop boop beep boop boop.",
-	            "streamUrl": "",
 	            "redirectionUrl": "www.storage-mart.com"
 	        }
 	    ]
