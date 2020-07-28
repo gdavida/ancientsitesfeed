0e747a48edfe6fa98ad28918fa1c1382b241843e
Davida Gaffney <gdavida@gmail.com>
2020-07-27 21:29:21 -0500

    Update index.js
    update content

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ server.get('/', (req, res) => {
 	.json(
 	    [
 			  {
-			    "uid": "ANCIENT_SITES_JSON_TTS_3",
-			    "updateDate": "2020-07-24T00:00:00.0Z",
-			    "titleText": "Cow Castle - Somerset, England",
-			    "mainText": "Cow Castle is an Iron Age hill fort in Somerset, England. It stands upon an isolated hilltop and with a single rampart and ditch, enclosing 0.9 hectares. Legend says that the hillfort was built by fairies to protect themselves against the earth.",
+			    "uid": "ANCIENT_SITES_JSON_TTS_4",
+			    "updateDate": "2020-07-27T00:00:00.0Z",
+			    "titleText": "Glastonbury Lake Village - Somerset, England",
+			    "mainText": "Glastonbury Lake Village was an Iron Age village, situated on a crannog or man made island in the southwestern English county of Somerset. It has been described as 'the best preserved prehistoric village ever found in the United Kingdom'. The site covered an area of 400 feet north to south by 300 feeteast to west. It was first constructed 250 B.C. by laying down timber and clay. Wooden houses and barns were then built on the clay base and occupied by up to 200 people at any time until the village was abandoned around 50 B.C.",
 			    "redirectionUrl": ""
 			   }
 	    ]
