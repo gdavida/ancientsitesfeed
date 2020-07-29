29177b79e78d6a60b4df408b7dcc3f715332ee23
Davida Gaffney <gdavida@gmail.com>
2020-07-29 09:10:22 -0500

    Update index.js
    content update

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ server.get('/', (req, res) => {
 	.json(
 	    [
 			  {
-			    "uid": "ANCIENT_SITES_JSON_TTS_4",
-			    "updateDate": "2020-07-27T00:00:00.0Z",
-			    "titleText": "Glastonbury Lake Village - Somerset, England",
-			    "mainText": "Glastonbury Lake Village was an Iron Age village, situated on a crannog or man made island in the southwestern English county of Somerset. It has been described as 'the best preserved prehistoric village ever found in the United Kingdom'. The site covered an area of 400 feet north to south by 300 feeteast to west. It was first constructed 250 B.C. by laying down timber and clay. Wooden houses and barns were then built on the clay base and occupied by up to 200 people at any time until the village was abandoned around 50 B.C.",
+			    "uid": "ANCIENT_SITES_JSON_TTS_5",
+			    "updateDate": "2020-07-29T00:00:00.0Z",
+			    "titleText": "Bryn Celli Ddu - Wales",
+			    "mainText": "Bryn Celli Ddu is a prehistoric site on the Welsh island of Anglesey. Its name means 'the mound in the dark grove' and is generally considered to be one of the finest passage tombs in Wales. Visitors can enter through a stone passage to the burial chamber. Within the burial chamber is a mysterious pillar, a reproduction of the 'Pattern Stone', carved with sinuous serpentine designs. The site was also once a henge with a stone circle, and may have been used to plot the date of the summer solstice.",
 			    "redirectionUrl": ""
 			   }
 	    ]
