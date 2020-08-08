7794863f6a1ff84807485fd39e8b3345bb39062f
Davida Gaffney <gdavida@gmail.com>
2020-08-08 11:29:58 -0500

    content update

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ server.get('/', (req, res) => {
 	.json(
 	    [
 			  {
-			    "uid": "ANCIENT_SITES_JSON_TTS_8",
-			    "updateDate": "2020-08-07T00:00:00.0Z",
-			    "titleText": "Glastonbury Lake Village in Somerset, England",
-			    "mainText": "Glastonbury Lake Village in Somerset, England covers a site 400 feet  north to south by 300 feet east to west. It was first constructed 250 B.C. by laying down timber and clay. Wooden houses and barns were then built on the clay base and occupied by up to 200 people at any time until the village was abandoned around 50 B.C. Glastonbury is considered the best preserved prehistoric village ever found in the United Kingdom.",
+			    "uid": "ANCIENT_SITES_JSON_TTS_9",
+			    "updateDate": "2020-08-08T00:00:00.0Z",
+			    "titleText": "Arbor Low in the Derbyshire Peak District, England",
+			    "mainText": "Arbor Low is a well-preserved Neolithic henge dating back 4000 years ago. About 50 large limestone blocks, quarried from a local site,  form an egg-shaped circle. There were probably 41-43 stones originally, but some are now in fragments.They range in size from about 5 ft to 7 ft, with monoliths of between 8 and a half to 9 and a half ft. One stone is partially upright; the rest are all lying flat. Although it is often stated that the stones have never stood upright, it is possible that they had originally been set upright in shallow stone holes. In the centre of the circle lie at least six smaller blocks, originally believed to have been set in a rectangle, known as the cove.",
 			    "redirectionUrl": ""
 			   }
 	    ]
