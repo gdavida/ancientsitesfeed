959c41a3b1516fffeee14887d670a74de2b698ad
Davida Gaffney <gdavida@gmail.com>
2020-08-12 23:49:49 -0500

    content update

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ server.get('/', (req, res) => {
 	.json(
 	    [
 			  {
-			    "uid": "ANCIENT_SITES_JSON_TTS_11",
-			    "updateDate": "2020-08-10T00:00:00.0Z",
-			    "titleText": "Mên-an-Tol in Cornwall, UK",
-			    "mainText": "The Mên-an-Tol is a small formation of standing stones in Cornwall, UK. It is also known locally as the "Crick Stone".	The Mên-an-Tol consists of three upright granite stones: a round stone with its middle holed out with two standing stones to each side, in front of and behind the hole. When seen at an angle from one side, the stones form a three-dimensional "101." The Mên-an-Tol is thought to date to 3500 years ago, and that the holed stone could have been a natural occurrence rather than deliberately sculpted. Mên-an-Tol is supposed to have a fairy or piskie guardian who can make miraculous cures.",
+			    "uid": "ANCIENT_SITES_JSON_TTS_12",
+			    "updateDate": "2020-08-12T00:00:00.0Z",
+			    "titleText": "Settlement Site Skara Brae in Scotland",
+			    "mainText": "The Settlement Site Skara Brae is a stone-built Neolithic settlement, located on the Bay of Skaill on the west coast of Mainland, the largest island in the Orkney archipelago of Scotland. Skara Brae consists of eight clustered houses, and was occupied from roughly 3180 BC to about 2500 BC. It is Europe's most complete Neolithic village. Skara Brae has been called the 'Scottish Pompeii' because of its excellent preservation."
 			    "redirectionUrl": ""
 			   }
 	    ]
