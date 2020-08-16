2e33d12a4ea42b932c9029c0facb8f918e738a65
Davida Gaffney <gdavida@gmail.com>
2020-08-16 09:07:39 -0500

    content reuse

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const express = require('express');
-const server = express();
-
-server.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
-
-
-server.get('/', (req, res) => {
-	res
-	.set('Content-Type', 'application/json')
+@@ -12,10 +12,10 @@ server.get('/', (req, res) => {
 	.json(
 	    [
 			  {
 			    "uid": "ANCIENT_SITES_JSON_TTS_12",
-			    "updateDate": "2020-08-12T00:00:00.0Z",
-			    "titleText": "Settlement Site Skara Brae in Scotland",
-			    "mainText": "The Settlement Site Skara Brae is a stone-built Neolithic settlement, located on the Bay of Skaill on the west coast of Mainland, the largest island in the Orkney archipelago of Scotland. Skara Brae consists of eight clustered houses, and was occupied from roughly 3180 BC to about 2500 BC. It is Europe's most complete Neolithic village. Skara Brae has been called the 'Scottish Pompeii' because of its excellent preservation."
+			    "updateDate": "2020-08-16T00:00:00.0Z",
+			    "titleText": "The Ridgeway - Southwestern England and Wales",
+			    "mainText": "For more than 5,000 years travellers have used the Ridgeway, Britain's oldest road. Stretching 85 miles, it functioned for thousands of years as a reliable trading route. The high dry ground made travel easy and provided a measure of protection by giving traders a commanding view, warning against potential attacks. During the Iron Age, inhabitants took advantage of the high ground by building hillforts along the Ridgeway to help defend the trading route. In medieval times and later, the Ridgeway was used to move livestock from the West Country and Wales to markets in London. Before the Enclosure Acts of 1750, the Ridgeway existed as an informal series of tracks across the chalk downs, chosen by travellers based on path conditions. Once enclosures started, the current path developed through the building of earth banks and the planting of hedges.",
 			    "redirectionUrl": ""
 			   }
 	    ]
-	)
-});
-
-server.listen(PORT, () => {
-     console.log("The server is listening.")
-});
